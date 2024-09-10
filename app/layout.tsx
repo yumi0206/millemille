8839578c8d1485a9fd5a6562879e33c9c230c563
yumi0206 <noraya026@gmail.com>
2024-09-10 21:05:00 +0900

    OGP設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,36 @@ import { zenOldMincho } from "@/lib/font";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
+const siteUrl =
+  process.env.NEXT_PUBLIC_URL || "https://www.millemille-kushiro.com";
+
 export const metadata: Metadata = {
   title: "Patisserie mille.mille",
   description: "釧路パティスリーミルミル",
   robots: {
     index: false,
+  },
+  openGraph: {
+    title: "Patisserie mille.mille",
+    description: "釧路パティスリーミルミル",
+    url: siteUrl,
+    siteName: "Patisserie mille.mille",
+    images: [
+      {
+        url: `${siteUrl}/OGP.png`,
+        width: 1200,
+        height: 630,
+        alt: "Patisserie mille.mille OGP Image",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Patisserie mille.mille",
+    description: "釧路パティスリーミルミル",
+    images: [`${siteUrl}/OGP.png`],
   },
 };
 
